5579a6170586da41c3544717a1717651ee18447b
svaltqt <naxxudea@gmail.com>
2022-12-01 16:22:34 -0500

    update random num unico

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,41 @@
 $(document).ready(function()
         { 
             cont=0;
-            var flag = new Boolean(false);
+            cont2=0;  
+            var numarray = [0,0,0,0,0,0,0,0,0,0];  
+    
+            function recorrecVector(num){
+            
+                for(i=0;i<10;i++)
+                {
+                    if(numarray[i] == num ){
+                        return 1;
+                    }                                
+                }
+                return 0;
+            }
 
             $("#agregar").click(function()
             {  
-                // Funcion generar numero random con dos args
+                // Funcion generar numero random no repetido
                 function getRandom(min, max) {
                     var x = Math.random()*(max - min)+min;               
                     var random = Math.round(x);  
                     return random;
-}
+                }              
+                numerosaletorios = getRandom(1,10); //2
+
+                while(recorrecVector(numerosaletorios)==1){
+                    alert(numerosaletorios);
+                    numerosaletorios = getRandom(1,10);
+
+                }numarray[cont] = numerosaletorios
+
                 /**Nombre: palabra "Usuario" concatenado con un valor aleatorio entre 1 y 10. 
                 Este nombre no se repetirá dentro de la tabla. */
-                
-                
-                // generar num aleatorio no repetido
-                                              
-                      
-                            
-                const aleatorios = []; 
-                function dropUnicos(){
-                    Narray = [1,2,3,4,5,6,7,8,9,10];                     
-                    var num = getRandom(1, 2);                    
+      
 
-                    for(var i=0;i<Narray.length; i++)
-                    {
-                        if(num ==Narray[i] ){
-                        aleatorios.push("Otro"+Narray[i])
-                        }else{
-                        aleatorios.push("Usuario"+Narray[i])
-                        }
-                        
-
-                    }
-                    return "listo";
-            }
-            var test = dropUnicos()
-         
+                //var numeros = 10;
                 // Generar  de género
                 function randomgen(){
                     numrandom = getRandom(1, 2);                     
@@ -47,7 +45,8 @@ $(document).ready(function()
                     };
                     if (numrandom == 1){return dict[1];}
                     else if (numrandom == 2){return dict[2];}                   
-                }var genero = randomgen();
+                }
+                genero = randomgen();
 
 
 
@@ -56,24 +55,21 @@ $(document).ready(function()
 
                
                 // Mostrar datos en html
-                if (cont<10){
-                    flag = true;
-                    
+                if (cont2<10){
+                                        
                     var add = "<tr>"
-                    add+="<td>"+aleatorios[cont]+"</td>"
+                    add+="<td>"+"Usuario"+numarray[cont]+"</td>"
                     add+="<td>"+genero+"</td>"
                     add+="<td>"+edad+"</td>"
                     add+="<tr>"                
-                    cont++;
-                }else if(cont==10){
+                    cont2++;
+                }else if(cont2==10){
                     // deshabilitar botton
                     $('#agregar').prop('disabled', true);
                 }
-                
-                          
-                           
-
+             
                 $("tbody").append(add);
             });
-        });
+        });    
 
+        
